8dc90db552310125acc5e7fce3c76c6f0614aa36
Leandro Rocha <prog.leandro@gmail.com>
2020-04-22 22:55:05 -0300

    Tipando objetos e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,14 @@
 import {  Request, Response} from 'express';
+import createUser from './services/CreateUsers';
 
 export function  helloWorld (req: Request, res: Response) {
+  
+  const user = createUser({
+    name: 'leandro',
+    email: 'Teste',
+    password: 'res',
+    techs: ['Node', 'React', 'Angular', { title: 'Ionic', experience:10}]
+  });
+
   return res.json({message: 'Hello World'});
 }
